a9d60c72f8f21ee0161e327940da29384dd03353
MisaDevs <misavazquezsifuentes@gmail.com>
2025-05-31 14:48:20 -0600

    Refresh tokens fix

--- src/routes/instagram/instagram.module.ts
+++ src/routes/instagram/instagram.module.ts
@@ -4,16 +4,9 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { InstagramController } from './controllers/instagram.controller';
 import { InstagramService } from './services/instagram.service';
 import { ReportModule } from 'src/reports/reports.module';
-import { AuthModule } from '../auth/auth.module';
-import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [
-    HttpModule,
-    forwardRef(() => ReportModule),
-    forwardRef(() => AuthModule),
-    UserModule,
-  ],
+  imports: [HttpModule, forwardRef(() => ReportModule)],
   providers: [InstagramService, PrismaService],
   controllers: [InstagramController],
   exports: [InstagramService],
